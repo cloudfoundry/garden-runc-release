6edc7cec47f6a972cfc71f8ce4f00624b93e0662
Tom Godkin <tgodkin@pivotal.io>
2017-10-25 09:56:26 +0100

    Reduce flakiness of greenskeeper suite
    [#152240930]

--- src/greenskeeper/greenskeeper_suite_test.go
+++ src/greenskeeper/greenskeeper_suite_test.go
@@ -2,12 +2,13 @@ package greenskeeper
 
 import (
 	"os/exec"
+	"os/user"
+	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
-
-	"testing"
 )
 
 func TestGreenskeeper(t *testing.T) {
@@ -16,13 +17,29 @@ func TestGreenskeeper(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-	cmd := exec.Command("groupadd", "mewtwo")
+	if _, err := user.LookupGroup("mewtwo"); err == user.UnknownGroupError("mewtwo") {
+		createGroup("mewtwo")
+	} else {
+		Expect(err).NotTo(HaveOccurred())
+	}
+
+	if _, err := user.Lookup("mew"); err == user.UnknownUserError("mew") {
+		createUser("mew", "mewtwo")
+	} else {
+		Expect(err).NotTo(HaveOccurred())
+	}
+})
+
+func createGroup(name string) {
+	cmd := exec.Command("groupadd", name)
 	session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 	Expect(err).NotTo(HaveOccurred())
-	Eventually(session).Should(gexec.Exit(0))
+	Eventually(session, time.Second*5).Should(gexec.Exit(0))
+}
 
-	cmd = exec.Command("useradd", "mew", "-G", "mewtwo")
-	session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+func createUser(name, group string) {
+	cmd := exec.Command("useradd", name, "-G", group)
+	session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 	Expect(err).NotTo(HaveOccurred())
-	Eventually(session).Should(gexec.Exit(0))
-})
+	Eventually(session, time.Second*5).Should(gexec.Exit(0))
+}
