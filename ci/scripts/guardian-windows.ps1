e18e037a510acb551681d5b747a6456ca3f5f845
Ed King <eking@pivotal.io>
2017-11-20 15:46:18 +0000

    Remove Golang install from guardian-windows CI job
    Golang now gets installed on the concourse Windows worker as part of
    windows-tools.
    
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- ci/scripts/guardian-windows.ps1
+++ ci/scripts/guardian-windows.ps1
@@ -6,19 +6,6 @@ cd gr-release-develop
 $env:GOPATH = $PWD
 $env:PATH = $env:GOPATH + "/bin;C:/go/bin;" + $env:PATH
 
-if ((Get-Command "go.exe" -ErrorAction SilentlyContinue) -eq $null) {
-  Write-Host "Installing Go 1.9.2!"
-  Invoke-WebRequest https://storage.googleapis.com/golang/go1.9.2.windows-amd64.msi -OutFile go.msi
-
-  $p = Start-Process -FilePath "msiexec" -ArgumentList "/passive /norestart /i go.msi" -Wait -PassThru
-
-  if($p.ExitCode -ne 0) {
-    throw "Golang MSI installation process returned error code: $($p.ExitCode)"
-  }
-
-  Write-Host "Go is installed!"
-}
-
 Write-Host "Installing Ginkgo"
 go.exe install ./src/github.com/onsi/ginkgo/ginkgo
 if ($LastExitCode -ne 0) {
