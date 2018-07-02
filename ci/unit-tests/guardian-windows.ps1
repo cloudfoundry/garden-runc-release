2cfc10a44f1c834f3abd1862443770f3f615f56f
Tom Godkin <tgodkin@pivotal.io>
2018-07-02 11:58:48 +0100

    Don't run runcontainerd/nerd tests on Windows
    [#156607857]

--- ci/unit-tests/guardian-windows.ps1
+++ ci/unit-tests/guardian-windows.ps1
@@ -19,7 +19,7 @@ go vet ./...
 Write-Host "compiling test process: $(date)"
 
 $env:GARDEN_TEST_ROOTFS = "N/A"
-ginkgo -r -nodes 8 -race -keepGoing -failOnPending -skipPackage "dadoo,gqt,kawasaki,locksmith,socket2me,signals"
+ginkgo -r -nodes 8 -race -keepGoing -failOnPending -skipPackage "dadoo,gqt,kawasaki,locksmith,socket2me,signals,runcontainerd\nerd"
 if ($LastExitCode -ne 0) {
     throw "Ginkgo run returned error code: $LastExitCode"
 }
