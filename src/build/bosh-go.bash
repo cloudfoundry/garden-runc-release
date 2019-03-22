b930ea5297ab588004df01ea8a9b3a0726b56716
Tom Godkin <tgodkin@pivotal.io>
2019-03-22 13:44:05 +0000

    Containerd packaging uses common go script

--- src/build/bosh-go.bash
+++ src/build/bosh-go.bash
@@ -1,6 +1,9 @@
-# These need to be defined since bosh compilation VMs don't set $HOME
+# This needs to be defined since bosh compilation VMs don't set $HOME.
 export GOCACHE=/tmp/gocache
-export GOPATH=/tmp/gopath
+
+# Since this is only to be sourced in bosh packaging scripts, we can assume
+# that ./gopath is a GOPATH.
+export GOPATH; GOPATH="$( readlink -nf ./gopath )"
 
 export GOROOT; GOROOT="$( readlink -nf /var/vcap/packages/golang )"
 export PATH="${GOROOT}/bin:${PATH}"
