bce01e195244b7ec7ea636dfdb270f8bc63c15d8
Tom Godkin <tomgodkin@pm.me>
2022-04-15 08:44:35 +0100

    Migrate greenskeeper to Ginkgo v2

--- src/greenskeeper/integration/greenskeeper_integration_test.go
+++ src/greenskeeper/integration/greenskeeper_integration_test.go
@@ -9,8 +9,7 @@ import (
 	"strconv"
 	"syscall"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
