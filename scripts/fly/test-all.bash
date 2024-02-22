c862d1546551782e615eac4975222dbc855213c1
Josh Russett <josh.russett@broadcom.com>
2024-02-22 23:34:09 +0000

    Fix fly/test-all.bash pathing

--- scripts/fly/test-all.bash
+++ scripts/fly/test-all.bash
@@ -4,7 +4,7 @@ set -eu
 set -o pipefail
 
 THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-CI="${THIS_FILE_DIR}/../../wg-app-platform-runtime-ci"
+CI="$(realpath "${THIS_FILE_DIR}/../../../wg-app-platform-runtime-ci")"
 . "$CI/shared/helpers/git-helpers.bash"
 REPO_NAME=$(git_get_remote_name)
 
