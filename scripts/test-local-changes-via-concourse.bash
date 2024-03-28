6059a237f75e4e3d8c0e4133a8b167a32555bf14
David Sabeti <david.sabeti@broadcom.com>
2024-03-28 16:31:41 +0000

    Fix script path
    Signed-off-by: David Sabeti <david.sabeti@broadcom.com>

--- scripts/test-local-changes-via-concourse.bash
+++ scripts/test-local-changes-via-concourse.bash
@@ -4,4 +4,4 @@ set -eu
 
 THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-${THIS_FILE_DIR}/docker/fly/test-all.bash "$@"
+${THIS_FILE_DIR}/fly/test-all.bash "$@"
