1e33cd7f7eb9ef5292c7ebb96ed32e0a4a0f85b7
Amelia Downs <amelia.downs@broadcom.com>
2025-10-01 14:29:35 +0000

    add generate-release-note script

---
+++ scripts/generate-release-notes.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CI="${THIS_FILE_DIR}/../../wg-app-platform-runtime-ci"
+. "$CI/shared/helpers/release-note-helpers.bash"
+. "$CI/shared/helpers/git-helpers.bash"
+unset THIS_FILE_DIR
+
+# ex. version_range="v0.343.0...v0.344.0"
+version_range="${1:?Please provide the start and end versions you want to generate release notes for './generate-release-notes.bash start_ref...end_ref' }"
+# ex. local_start_ref="v0.343.0"
+local_start_ref=$(get_start_ref_from_range "${version_range}")
+# ex. local_end_ref="v0.344.0"
+local_end_ref=$(get_end_ref_from_range "${version_range}")
+
+BLOBS_LOCATION="config/blobs.yml";
+
+get_non_bot_commits "${local_start_ref}" "${local_end_ref}"
+echo ""
+
+display_blob_change_info "${local_start_ref}" "${local_end_ref}" "${BLOBS_LOCATION}"
+echo ""
+
+for repo in guardian garden garden-integration-tests grootfs idmapper dontpanic garden-performance-acceptance-tests containerd; do
+  START_REF_REPO=$(git rev-parse "${local_start_ref}:src/${repo}")
+  END_REF_REPO=$(git rev-parse "${local_end_ref}:src/${repo}")
+  pushd src/${repo} > /dev/null
+    get_non_bot_commits "${START_REF_REPO}" "${END_REF_REPO}" "${repo}"
+    display_go_mod_diff "${START_REF_REPO}" "${END_REF_REPO}" "go.mod" "${repo}"
+  popd > /dev/null
+  echo ""
+done
+
