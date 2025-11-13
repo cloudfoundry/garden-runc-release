8b58aa385ca08e5b43f081df090586f99bb06822
Amelia Downs <amelia.downs@broadcom.com>
2025-11-13 17:43:02 +0000

    update generate-release-notes
    the helper has been updated to handle whitespace itself

--- scripts/generate-release-notes.bash
+++ scripts/generate-release-notes.bash
@@ -18,19 +18,15 @@ local_end_ref=$(get_end_ref_from_range "${version_range}")
 
 BLOBS_LOCATION="config/blobs.yml";
 
-get_non_bot_commits "${local_start_ref}" "${local_end_ref}"
-echo ""
-
+display_non_bot_commits "${local_start_ref}" "${local_end_ref}"
 display_blob_change_info "${local_start_ref}" "${local_end_ref}" "${BLOBS_LOCATION}"
-echo ""
 
 for repo in guardian garden garden-integration-tests grootfs idmapper dontpanic garden-performance-acceptance-tests containerd; do
   START_REF_REPO=$(git rev-parse "${local_start_ref}:src/${repo}")
   END_REF_REPO=$(git rev-parse "${local_end_ref}:src/${repo}")
   pushd src/${repo} > /dev/null
-    get_non_bot_commits "${START_REF_REPO}" "${END_REF_REPO}" "${repo}"
+    display_non_bot_commits "${START_REF_REPO}" "${END_REF_REPO}" "${repo}"
     display_go_mod_diff "${START_REF_REPO}" "${END_REF_REPO}" "go.mod" "${repo}"
   popd > /dev/null
-  echo ""
 done
 
